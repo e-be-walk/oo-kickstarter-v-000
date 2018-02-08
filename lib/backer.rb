6cf87581a32b429286fe07e89dca09dedc544e62
Erin Walker <erinb0317@hotmail.com>
2018-02-08 02:00:51 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,10 @@
 class Backer
   attr_accessor :backed_projects
-
-  def initialize
-    @backed_projects = []
+  @@backed_projects = []
+  
+  def initialize(name)
+    @name = name
+    
   end
 
 end
